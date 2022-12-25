863999f65777044afad65d3ba628406c616069d2
Koushik Reddy Gajjala <koushikreddy2005@outlook.com>
2022-12-24 22:40:49 -0800

    Specific Tasks can be deleted with a dedicated delete button

--- JSLearn/app.js
+++ JSLearn/app.js
@@ -10,21 +10,53 @@ loadEventListeners();
 
 // Load all Event Listners
 function loadEventListeners() {
+   // DOM Load Event
+   document.addEventListener('DOMContentLoaded', getTasks);
   // Add task Event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear tasks
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Tasks from LS
+function getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task) {
+        //create a li element
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(task));
+
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
 }
 
 //Add Task
 function addTask(e) {
   // If no input is given then throw a alert
   if(taskInput.value === '') {
-    alert("Enter Task");
+    alert("Add a Task");
+    return;
   }
 
-  //else 
   //This is a dummy comment
   //Make a li
-    const li = document.createElement('l1');
+    const li = document.createElement('li');
 
   // Give it a class 
     li.className = 'collection-item';
@@ -42,15 +74,72 @@ function addTask(e) {
 
     //add icon to html
     link.innerHTML = '<i class="fa fa-remove"></i>'
-    li.append(link);
-    console.log(li);
+    li.appendChild(link);
 
     //now append li to ul
-    taskList.append(li);
+    taskList.appendChild(li);
+
+    // Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+
+    // When the task is over then clear the input 
+    taskInput.value = '';
 
   //when the button is clicked then the page gets automatically redirected
   //to stop that, stop the default behaviour of the event
-
   e.preventDefault();
 
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+
+}
+
+// Clear Tasks
+function clearTasks(e) {
+    // taskList.innerHTML = '';
+
+    // Faster idk why?
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    
+    document.querySelectorAll('.collection-item').forEach(
+        //task is the <li> element
+        function(task) {
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            }
+            else {
+                task.style.display = 'none';
+            }
+        }
+    );
 }
